0393d71c354ba2ec85c6b089b7896041d63ae5a6
reiworkout-droid <rei.workout@gmail.com>
2026-02-25 22:45:38 +0900

    ユーザー認証のAPI連携

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\AuthController;
+
+// 登録APIのルート
+Route::post('/register', [AuthController::class, 'register']);
+
+// ログインAPIのルート
+Route::post('/login', [AuthController::class, 'login']);
+
+// ログアウトAPIのルート
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
+// 認証されたユーザーの情報を取得するAPIのルート
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
