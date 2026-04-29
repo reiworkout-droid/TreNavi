e833674b8ff091ba3f64b32872626bd77977a714
reiworkout-droid <rei.workout@gmail.com>
2026-04-29 22:55:16 +0900

    診断結果のDB再設計

---
+++ app/Http/Controllers/DiagnosisController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Diagnosis;
+use Illuminate\Http\Request;
+
+class DiagnosisController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Diagnosis $diagnosis)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Diagnosis $diagnosis)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Diagnosis $diagnosis)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Diagnosis $diagnosis)
+    {
+        //
+    }
+}
---
+++ app/Models/Diagnosis.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Diagnosis extends Model
+{
+    //
+    // public function user()
+    // {
+    //     return $this->belongsTo(User::class);
+    // }
+}
