9713b830594121c844c4adbdeb6f98a4ca8bbc8d
reiworkout-droid <rei.workout@gmail.com>
2026-05-02 15:18:18 +0900

    診断ロジックの変更

--- app/Models/Diagnosis.php
+++ app/Models/Diagnosis.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Diagnosis extends Model
 {
@@ -15,4 +16,32 @@ class Diagnosis extends Model
         'pace',
         'distance',
     ];
+
+    // JSON変換時に一緒に診断結果を含める
+        protected $appends = ['user_type'];
+
+    /**
+     * 数値からタイプを判定するロジック 
+     */
+    protected function userType(): Attribute
+    {
+        return Attribute::make(
+            get: function () {
+                if ($this->style >= 4 && $this->logic >= 4 && $this->pace >= 4 && $this->distance >= 3) {
+                    return "ストイック型";
+                }
+                if ($this->style <= 2 && $this->talk >= 4 && $this->pace <= 3 && $this->distance <= 2) {
+                    return "エンジョイ型";
+                }
+                if ($this->style <= 2 && $this->distance <= 2 && $this->talk >= 3) {
+                    return "サポート重視型";
+                }
+                if ($this->talk <= 2 && $this->pace <= 2) {
+                    return "マイペース型";
+                }
+
+                return "バランス型";
+            },
+        );
+    }
 }
